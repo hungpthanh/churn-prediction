577755cbe8a31f9eb6bef3ec98510d3a128f9915
Pham Thanh Hung <hungthanhpham94@gmail.com>
2019-09-05 00:36:37 -0700

    refactor

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -6,16 +6,22 @@ from sklearn.model_selection import train_test_split
 import lib
 import numpy as np
 
+
 def clear_data(df, args):
+    """
+    Slipting data to train and test dataset
+    :param df: Pandas Frame
+    :param args: argument
+    :return: slipted data
+    """
     df = df.drop(CUSTOMER_ID, 1)
     df = df[~(df.TotalCharges == " ")]  # delete empty string
-    df.TotalCharges = pd.to_numeric(df.TotalCharges)
+    df.TotalCharges = pd.to_numeric(df.TotalCharges) # change value to number
     df, dict_label_encoders = lib.normalize_data(df, discrete_features)
     df_train, df_test = train_test_split(df, test_size=0.25, random_state=args.seed)
-    df_train_input = df_train.iloc[:, :-3]
-    print(df_train_input.head)
+    df_train_input = df_train.iloc[:, :-1]  # -1: use all feature, -3: use discrete feature
     df_train_target = np.array(df_train[CHURN])
-    df_test_input = df_test.iloc[:, :-3]
+    df_test_input = df_test.iloc[:, :-1]
     df_test_target = np.array(df_test[CHURN])
     scaler = StandardScaler()
     df_train_input_sc = scaler.fit_transform(df_train_input)
@@ -24,6 +30,12 @@ def clear_data(df, args):
 
 
 def normalize_data(df, features):
+    """
+    Digitizing data
+    :param df: Pandas frame
+    :param features: discrete feature
+    :return: digitized data
+    """
     dict_label_encoder = {}
     for feature in features:
         dict_label_encoder[feature] = LabelEncoder()
--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 from sklearn import tree
 from sklearn.metrics import classification_report, accuracy_score
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--algo', default='decisiontree', type=str)
+parser.add_argument('--algo', default='FFN', type=str)
 parser.add_argument("--batchsize", type=int, default=64)
 parser.add_argument("--input_dim", type=int, default=19)
 parser.add_argument("--output_dim", type=int, default=125)
@@ -52,6 +53,7 @@ def main():
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
         dataloader = lib.DataLoader(df_train_input_sc, df_train_target, args.batchsize)
 
+        print(df_train_input_sc.shape)
         # training
         model.train()
         for epoch in range(args.num_epochs):
@@ -61,13 +63,10 @@ def main():
                 cnt += 1
                 input_data = torch.Tensor(input_data)
                 target_data = torch.LongTensor(target_data)
-                # print(input_data.size())
-                # print(target_data.size())
                 optimizer.zero_grad()
                 logit = model(input_data)
                 loss = F.nll_loss(logit, target_data)
                 pred = logit.data.max(1)[1]
-                # print("iterator: {}, loss = {}".format(it, loss.item()))
                 sum_loss += loss.item()
                 loss.backward()
                 optimizer.step()
@@ -83,9 +82,6 @@ def main():
             y_pred_dtc = logit.data.max(1)[1]
 
     print(classification_report(df_test_target, y_pred_dtc))
-    # print(accuracy_score(df_test_target, y_pred_dtc))
-    # print(type(y_pred_dtc))
-    # print(y_pred_dtc.shape)
 
 
 if __name__ == '__main__':
